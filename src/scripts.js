9e7b1a5a194261b6c7f752cee72d3a16561eaced
UcheSylvester <okorocode@gmail.com>
2019-07-26 14:31:54 +0100

    displaying cards

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,9 @@
 console.log('working')
 
+const mainContainer = document.querySelector('main.container')
+console.log(mainContainer)
+
+
 const fetchCards = () => {
     fetch('data.json')
         .then(response => response.json())
@@ -7,9 +11,31 @@ const fetchCards = () => {
         .catch(err => console.log(err))
 }
 
-const displayCards = (data) => {
-    console.log(data)
+const displayCards = (persons) => {
+    let htmlContent = '';
+    if(persons) {
+        htmlContent = persons.map(({ number, fullName, imageLink, title, links }) => `
+        <section class="card">
+            <div class="main-info">
+                <img class="image" src="${imageLink}" alt="">
+
+                <h2 class="full-name">${fullName}</h2>
+                <p class="title">${title}</p>
+            </div>
+            <div class="social-media">
+                <a href="" target="_blank"><i class="material-icons">face</i></a>
+                <a><i class="material-icons">face</i></a>
+                <a href="" target="_blank"><i class="material-icons">face</i></a>
+            </div>
+            <p class="location">Aba, Abia State</p>
+        </section>
+
+        `).join('')
+    }
+
+    mainContainer.insertAdjacentHTML("beforeend", htmlContent)
 }
 
 
 document.addEventListener('DOMContentLoaded', fetchCards);
+ 
